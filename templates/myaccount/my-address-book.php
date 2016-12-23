7a6a8f636fd46f7afc2d72aa8a6e09e1794912bb
Gary Thayer <garythayer@Garys-Mac-mini.local>
2016-12-23 10:16:52 -0500

    reformating template page

--- templates/myaccount/my-address-book.php
+++ templates/myaccount/my-address-book.php
@@ -17,69 +17,70 @@ $customer_id = get_current_user_id();
 $address_book = $WC_Address_Book->get_address_book( $customer_id );
 
 // Do not display on address edit pages.
-if ( ! $type ) {
+if ( ! $type ) : ?>
 
+	<?php 
 	// Only display if primary addresses are set and not on an edit page.
-	if ( count ( $address_book ) > 1 ) {
+	if ( count ( $address_book ) > 1 ) : ?>
 
-		echo '<hr />';
+		<hr />
 
-		echo '<div class="address_book">'; ?>
+		<div class="address_book">
 
-		<h2><?php _e( 'Shipping Address Book', 'woocommerce' ); ?></h2>
+			<h2><?php _e( 'Shipping Address Book', 'woocommerce' ); ?></h2>
 
-		<p class="myaccount_address">
-			<?php echo apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woocommerce' ) ); ?>
-		</p>
+			<p class="myaccount_address">
+				<?php echo apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woocommerce' ) ); ?>
+			</p>
 
-		<?php if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) echo '<div class="col2-set addresses address-book">';
+			<?php if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) echo '<div class="col2-set addresses address-book">';
 
-		foreach ( $address_book as $name => $fields ) :
+			foreach ( $address_book as $name => $fields ) :
 
-			// Prevent default shipping from displaying here.
-			if ( 'shipping' === $name ) {
-				continue;
-			}
+				// Prevent default shipping from displaying here.
+				if ( 'shipping' === $name ) {
+					continue;
+				}
 
-			?>
+				$address = apply_filters( 'woocommerce_my_account_my_address_formatted_address', array(
+					'first_name'  => get_user_meta( $customer_id, $name . '_first_name', true ),
+					'last_name'   => get_user_meta( $customer_id, $name . '_last_name', true ),
+					'company'     => get_user_meta( $customer_id, $name . '_company', true ),
+					'address_1'   => get_user_meta( $customer_id, $name . '_address_1', true ),
+					'address_2'   => get_user_meta( $customer_id, $name . '_address_2', true ),
+					'city'        => get_user_meta( $customer_id, $name . '_city', true ),
+					'state'       => get_user_meta( $customer_id, $name . '_state', true ),
+					'postcode'    => get_user_meta( $customer_id, $name . '_postcode', true ),
+					'country'     => get_user_meta( $customer_id, $name . '_country', true )
+				), $customer_id, $name );
 
-			<div class="wc-address-book-address">
-				<div class="wc-address-book-meta">
-					<a href="<?php echo wc_get_endpoint_url( 'edit-address', $name . '/' ); ?>" class="wc-address-book-edit"><?php _e( 'Edit', 'woocommerce' ); ?></a>
-					<a id="<?php echo $name ?>" class="wc-address-book-delete"><?php _e( 'Delete', 'woocommerce' ); ?></a>
-					<a id="<?php echo $name ?>" class="wc-address-book-make-primary"><?php _e( 'Make Primary', 'woocommerce' ); ?></a>
-				</div>
-				<address>
-					<?php
-						$address = apply_filters( 'woocommerce_my_account_my_address_formatted_address', array(
-							'first_name'  => get_user_meta( $customer_id, $name . '_first_name', true ),
-							'last_name'   => get_user_meta( $customer_id, $name . '_last_name', true ),
-							'company'     => get_user_meta( $customer_id, $name . '_company', true ),
-							'address_1'   => get_user_meta( $customer_id, $name . '_address_1', true ),
-							'address_2'   => get_user_meta( $customer_id, $name . '_address_2', true ),
-							'city'        => get_user_meta( $customer_id, $name . '_city', true ),
-							'state'       => get_user_meta( $customer_id, $name . '_state', true ),
-							'postcode'    => get_user_meta( $customer_id, $name . '_postcode', true ),
-							'country'     => get_user_meta( $customer_id, $name . '_country', true )
-						), $customer_id, $name );
+				$formatted_address = WC()->countries->get_formatted_address( $address );
 
-						$formatted_address = WC()->countries->get_formatted_address( $address );
+				if ( $formatted_address ) : ?>
 
-						if ( ! $formatted_address )
-							_e( 'You have not set up this address yet.', 'woocommerce' );
-						else
-							echo $formatted_address;
-					?>
-				</address>
-			</div>
+					<div class="wc-address-book-address">
+						<div class="wc-address-book-meta">
+							<a href="<?php echo wc_get_endpoint_url( 'edit-address', $name . '/' ); ?>" class="wc-address-book-edit"><?php _e( 'Edit', 'woocommerce' ); ?></a>
+							<a id="<?php echo $name ?>" class="wc-address-book-delete"><?php _e( 'Delete', 'woocommerce' ); ?></a>
+							<a id="<?php echo $name ?>" class="wc-address-book-make-primary"><?php _e( 'Make Primary', 'woocommerce' ); ?></a>
+						</div>
+						<address>
+							<?php echo $formatted_address; ?>
+						</address>
+					</div>
 
-		<?php endforeach;
+				<?php endif; ?>
 
-		echo '</div>';
-	}
+			<?php endforeach; ?>
 
-	// Add link/button to the my accounts page for adding addresses.
-	if ( null != get_user_meta( $customer_id, 'shipping_address_1' ) ) {
-		$WC_Address_Book->add_additional_address_button();
-	}
-}
+		</div>
+	<?php endif; ?>
+
+	<?php
+		// Add link/button to the my accounts page for adding addresses.
+		if ( null != get_user_meta( $customer_id, 'shipping_address_1' ) ) {
+			$WC_Address_Book->add_additional_address_button();
+		}
+	?>
+
+<?php endif; ?>
