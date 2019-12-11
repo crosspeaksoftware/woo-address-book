0d96e0af1e6b2e034ea8e1c8305bfd3204622a06
Matt Harrison <matt@hallme.com>
2019-12-11 16:27:28 -0500

    setting up the woocommerce tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,12 +20,62 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 /**
+ * Returns WooCommerce main directory.
+ *
+ * @return string
+ */
+function wc_dir() {
+	return dirname( dirname( dirname( __FILE__ ) ) ) . '/woocommerce';
+}
+
+/**
+ * Adds WooCommerce testing framework classes.
+ */
+function wc_test_includes() {
+	$wc_tests_framework_base_dir = wc_dir() . '/tests';
+	// WooCommerce test classes.
+	// Framework.
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-unit-test-factory.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-session-handler.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-wc-data.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-wc-object-query.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-payment-gateway.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-payment-token-stub.php';
+	require_once $wc_tests_framework_base_dir . '/framework/vendor/class-wp-test-spy-rest-server.php';
+	// Test cases.
+	require_once $wc_tests_framework_base_dir . '/includes/wp-http-testcase.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-unit-test-case.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-api-unit-test-case.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-rest-unit-test-case.php';
+	// Helpers.
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-product.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-coupon.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-fee.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-shipping.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-customer.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-order.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-shipping-zones.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-payment-token.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-settings.php';
+}
+
+
+/**
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
+	define( 'WC_TAX_ROUNDING_MODE', 'auto' );
+	define( 'WC_USE_TRANSACTIONS', false );
+	update_option( 'woocommerce_enable_coupons', 'yes' );
+	update_option( 'woocommerce_calc_taxes', 'yes' );
+
+	require_once wc_dir() . '/woocommerce.php';
+
 	require dirname( dirname( __FILE__ ) ) . '/woocommerce-address-book.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+wc_test_includes();
