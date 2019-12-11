8745857c30e667c058b2fbc7def70bdf877fd6ff
Matt Harrison <matt@hallme.com>
2019-12-11 15:31:38 -0500

    get all phpcs rules passing

--- templates/myaccount/my-address-book.php
+++ templates/myaccount/my-address-book.php
@@ -13,18 +13,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $wc_address_book = WC_Address_Book::get_instance();
 
-$customer_id  = get_current_user_id();
-$address_book = $wc_address_book->get_address_book( $customer_id );
+$woo_address_book_customer_id  = get_current_user_id();
+$woo_address_book_address_book = $wc_address_book->get_address_book( $woo_address_book_customer_id );
 
 // Do not display on address edit pages.
 if ( ! $type ) : ?>
 
 	<?php
 
-	$shipping_address = get_user_meta( $customer_id, 'shipping_address_1', true );
+	$woo_address_book_shipping_address = get_user_meta( $woo_address_book_customer_id, 'shipping_address_1', true );
 
 	// Only display if primary addresses are set and not on an edit page.
-	if ( ! empty( $shipping_address ) ) :
+	if ( ! empty( $woo_address_book_shipping_address ) ) :
 		?>
 
 		<hr />
@@ -42,43 +42,43 @@ if ( ! $type ) : ?>
 				echo '<div class="col2-set addresses address-book">';
 			}
 
-			foreach ( $address_book as $name => $fields ) :
+			foreach ( $woo_address_book_address_book as $woo_address_book_name => $woo_address_book_fields ) :
 
 				// Prevent default shipping from displaying here.
-				if ( 'shipping' === $name || 'billing' === $name ) {
+				if ( 'shipping' === $woo_address_book_name || 'billing' === $woo_address_book_name ) {
 					continue;
 				}
 
-				$address = apply_filters(
+				$woo_address_book_address = apply_filters(
 					'woocommerce_my_account_my_address_formatted_address',
 					array(
-						'first_name' => get_user_meta( $customer_id, $name . '_first_name', true ),
-						'last_name'  => get_user_meta( $customer_id, $name . '_last_name', true ),
-						'company'    => get_user_meta( $customer_id, $name . '_company', true ),
-						'address_1'  => get_user_meta( $customer_id, $name . '_address_1', true ),
-						'address_2'  => get_user_meta( $customer_id, $name . '_address_2', true ),
-						'city'       => get_user_meta( $customer_id, $name . '_city', true ),
-						'state'      => get_user_meta( $customer_id, $name . '_state', true ),
-						'postcode'   => get_user_meta( $customer_id, $name . '_postcode', true ),
-						'country'    => get_user_meta( $customer_id, $name . '_country', true ),
+						'first_name' => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_first_name', true ),
+						'last_name'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_last_name', true ),
+						'company'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_company', true ),
+						'address_1'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_1', true ),
+						'address_2'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_2', true ),
+						'city'       => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_city', true ),
+						'state'      => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_state', true ),
+						'postcode'   => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_postcode', true ),
+						'country'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_country', true ),
 					),
-					$customer_id,
-					$name
+					$woo_address_book_customer_id,
+					$woo_address_book_name
 				);
 
-				$formatted_address = WC()->countries->get_formatted_address( $address );
+				$woo_address_book_formatted_address = WC()->countries->get_formatted_address( $woo_address_book_address );
 
-				if ( $formatted_address ) :
+				if ( $woo_address_book_formatted_address ) :
 					?>
 
 					<div class="wc-address-book-address">
 						<div class="wc-address-book-meta">
-							<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $name ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
+							<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $woo_address_book_name ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
+							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
+							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
 						</div>
 						<address>
-							<?php echo wp_kses( $formatted_address, array( 'br' => array() ) ); ?>
+							<?php echo wp_kses( $woo_address_book_formatted_address, array( 'br' => array() ) ); ?>
 						</address>
 					</div>
 
@@ -93,13 +93,8 @@ if ( ! $type ) : ?>
 			?>
 
 		</div>
-	<?php endif; ?>
-
-	<?php
-	// Add link/button to the my accounts page for adding addresses.
-	if ( ! empty( get_user_meta( $customer_id, 'shipping_address_1' ) ) ) {
+		<?php
+		// Add link/button to the my accounts page for adding addresses.
 		$wc_address_book->add_additional_address_button();
-	}
-	?>
-
-<?php endif; ?>
+	endif;
+endif;
