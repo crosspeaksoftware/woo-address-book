134a4fdb2dea99d93f6e752ad94f23f6a731b8a4
Matt Harrison <matt@hallme.com>
2020-03-25 17:51:28 -0400

    add comment about copying the address book file to the theme

--- templates/myaccount/my-address-book.php
+++ templates/myaccount/my-address-book.php
@@ -2,6 +2,14 @@
 /**
  * Woo Address Book
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/my-address-book.php.
+ *
+ * HOWEVER, on occasion Woo Address Book will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
  * @package WooCommerce Address Book/Templates
  * @version 1.5.0
  */
