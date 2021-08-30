9f31383f6bfa43e93ef5cd9099dc3eee41061698
Karel Hink <info@karelhink.cz>
2021-08-30 10:34:20 +0200

    Added plugin settings to allow disable use of the billing or shipping address book

---
+++ includes/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * WooCommerce Address Book.
+ *
+ * @version  1.8.0
+ * @package  WooCommerce Address Book
+ */
+
+add_filter('woocommerce_general_settings', 'wc_address_book_general_settings');
+function wc_address_book_general_settings($settings) {
+
+    $settings[] = array( 'title' => __( 'WooCommerce Address Book options', 'woo-address-book' ), 'type' => 'title', 'desc' => __( 'Setting options for WooCommerce Address Book.', 'woo-address-book' ), 'id' => 'woo_address_book_options' );
+
+    $settings[] = array(
+        'title'   => __( 'Billing address book', 'woo-address-book' ),
+        'desc'    => __( 'Enable billing address book', 'woo-address-book' ),
+        'id'      => 'woo_address_book_billing_enable',
+        'default' => 'yes',
+        'type'    => 'checkbox',
+    );
+	$settings[] = array(
+        'title'   => __( 'Shipping address book', 'woo-address-book' ),
+        'desc'    => __( 'Enable shipping address book', 'woo-address-book' ),
+        'id'      => 'woo_address_book_shipping_enable',
+        'default' => 'yes',
+        'type'    => 'checkbox',
+    );
+
+    $settings[] = array( 'type' => 'sectionend', 'id' => 'woo_address_book_options' );
+
+    return $settings;
+}
--- templates/myaccount/my-address-book.php
+++ templates/myaccount/my-address-book.php
@@ -13,169 +13,160 @@
  * @package WooCommerce Address Book/Templates
  * @version 1.8.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
 $wc_address_book = WC_Address_Book::get_instance();
 
-$woo_address_book_customer_id  = get_current_user_id();
-$woo_address_book_billing_address_book = $wc_address_book->get_address_book( $woo_address_book_customer_id, 'billing' );
-$woo_address_book_shipping_address_book = $wc_address_book->get_address_book( $woo_address_book_customer_id, 'shipping' );
+$woo_address_book_customer_id			 = get_current_user_id();
+$woo_address_book_billing_address_book	 = $wc_address_book->get_address_book( $woo_address_book_customer_id, 'billing' );
+$woo_address_book_shipping_address_book	 = $wc_address_book->get_address_book( $woo_address_book_customer_id, 'shipping' );
 
 // Do not display on address edit pages.
-if ( ! $type ) : ?>
+if ( !$type ) {
+	if ( $wc_address_book->get_wcab_option( 'billing_enable' ) === true ) {
+		$woo_address_book_billing_address = get_user_meta( $woo_address_book_customer_id, 'billing_address_1', true );
 
-	<?php
-
-	$woo_address_book_billing_address = get_user_meta( $woo_address_book_customer_id, 'billing_address_1', true );
-
-	// Only display if primary addresses are set and not on an edit page.
-	if ( ! empty( $woo_address_book_billing_address ) ) :
-		?>
-
-		<hr />
-
-		<div class="address_book billing_address_book">
-
-			<h3><?php esc_html_e( 'Billing Address Book', 'woo-address-book' ); ?></h3>
-
-			<p class="myaccount_address">
-				<?php echo esc_html( apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woo-address-book' ) ) ); ?>
-			</p>
-
-			<?php
-			echo '<div class="col2-set addresses address-book">';
-
-			foreach ( $woo_address_book_billing_address_book as $woo_address_book_name => $woo_address_book_fields ) :
-
-				// Prevent default shipping from displaying here.
-				if ( 'billing' === $woo_address_book_name ) {
-					continue;
-				}
-
-				$woo_address_book_address = apply_filters(
-					'woocommerce_my_account_my_address_formatted_address',
-					array(
-						'first_name' => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_first_name', true ),
-						'last_name'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_last_name', true ),
-						'company'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_company', true ),
-						'address_1'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_1', true ),
-						'address_2'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_2', true ),
-						'city'       => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_city', true ),
-						'state'      => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_state', true ),
-						'postcode'   => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_postcode', true ),
-						'country'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_country', true ),
-					),
-					$woo_address_book_customer_id,
-					$woo_address_book_name
-				);
-
-				$woo_address_book_formatted_address = WC()->countries->get_formatted_address( $woo_address_book_address );
-
-				if ( $woo_address_book_formatted_address ) :
-					?>
-
-					<div class="wc-address-book-address">
-						<div class="wc-address-book-meta">
-							<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $woo_address_book_name, 'billing' ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
-						</div>
-						<address>
-							<?php echo wp_kses( $woo_address_book_formatted_address, array( 'br' => array() ) ); ?>
-						</address>
-					</div>
-
-				<?php endif; ?>
-
-			<?php endforeach; ?>
-
-			<?php
-			echo '</div>';
+		// Only display if primary addresses are set and not on an edit page.
+		if ( !empty( $woo_address_book_billing_address ) ) {
 			?>
 
-		</div>
-		<?php
-		// Add link/button to the my accounts page for adding addresses.
-		$wc_address_book->add_additional_address_button( 'billing' );
-	endif;
-
-	$woo_address_book_shipping_address = get_user_meta( $woo_address_book_customer_id, 'shipping_address_1', true );
-
-	// Only display if primary addresses are set and not on an edit page.
-	if ( ! empty( $woo_address_book_shipping_address ) ) :
-		?>
-
-		<hr />
-
-		<div class="address_book shipping_address_book">
-
-			<h3><?php esc_html_e( 'Shipping Address Book', 'woo-address-book' ); ?></h3>
-
-			<p class="myaccount_address">
-				<?php echo esc_html( apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woo-address-book' ) ) ); ?>
-			</p>
-
-			<?php
-			if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
-				echo '<div class="col2-set addresses address-book">';
-			}
-
-			foreach ( $woo_address_book_shipping_address_book as $woo_address_book_name => $woo_address_book_fields ) :
-
-				// Prevent default shipping from displaying here.
-				if ( 'shipping' === $woo_address_book_name ) {
-					continue;
-				}
-
-				$woo_address_book_address = apply_filters(
-					'woocommerce_my_account_my_address_formatted_address',
-					array(
-						'first_name' => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_first_name', true ),
-						'last_name'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_last_name', true ),
-						'company'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_company', true ),
-						'address_1'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_1', true ),
-						'address_2'  => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_2', true ),
-						'city'       => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_city', true ),
-						'state'      => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_state', true ),
-						'postcode'   => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_postcode', true ),
-						'country'    => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_country', true ),
-					),
-					$woo_address_book_customer_id,
-					$woo_address_book_name
-				);
-
-				$woo_address_book_formatted_address = WC()->countries->get_formatted_address( $woo_address_book_address );
-
-				if ( $woo_address_book_formatted_address ) :
+			<div class="address_book billing_address_book">
+				<header>
+					<h3><?php esc_html_e( 'Billing Address Book', 'woo-address-book' ); ?></h3>
+					<?php
+					// Add link/button to the my accounts page for adding addresses.
+					$wc_address_book->add_additional_address_button( 'billing' );
 					?>
+				</header>
 
-					<div class="wc-address-book-address">
-						<div class="wc-address-book-meta">
-							<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $woo_address_book_name, 'shipping' ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
-							<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
-						</div>
-						<address>
-							<?php echo wp_kses( $woo_address_book_formatted_address, array( 'br' => array() ) ); ?>
-						</address>
-					</div>
+				<p class="myaccount_address"><?php echo esc_html( apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woo-address-book' ) ) ); ?></p>
+				<div class="col2-set addresses address-book">
+					<?php
+					foreach ( $woo_address_book_billing_address_book as $woo_address_book_name => $woo_address_book_fields ) {
+						// Prevent default billing from displaying here.
+						if ( 'billing' === $woo_address_book_name ) {
+							continue;
+						}
 
-				<?php endif; ?>
+						$woo_address_book_address = apply_filters(
+							'woocommerce_my_account_my_address_formatted_address',
+							array(
+								'first_name' => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_first_name', true ),
+								'last_name'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_last_name', true ),
+								'company'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_company', true ),
+								'address_1'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_1', true ),
+								'address_2'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_2', true ),
+								'city'		 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_city', true ),
+								'state'		 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_state', true ),
+								'postcode'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_postcode', true ),
+								'country'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_country', true ),
+							),
+							$woo_address_book_customer_id,
+							$woo_address_book_name
+						);
 
-			<?php endforeach; ?>
+						$woo_address_book_formatted_address = WC()->countries->get_formatted_address( $woo_address_book_address );
 
+						if ( $woo_address_book_formatted_address ) {
+							?>
+
+							<div class="wc-address-book-address">
+								<div class="wc-address-book-meta">
+									<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $woo_address_book_name, 'billing' ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
+									<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
+									<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
+								</div>
+								<address>
+									<?php echo wp_kses( $woo_address_book_formatted_address, array('br' => array()) ); ?>
+								</address>
+							</div>
+
+							<?php
+						}
+					}
+					?>
+				</div>
+			</div>
 			<?php
-			if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
-				echo '</div>';
-			}
+		}
+	}
+
+	if ( $wc_address_book->get_wcab_option( 'shipping_enable' ) === true ) {
+		$woo_address_book_shipping_address = get_user_meta( $woo_address_book_customer_id, 'shipping_address_1', true );
+
+		// Only display if primary addresses are set and not on an edit page.
+		if ( !empty( $woo_address_book_shipping_address ) ) {
 			?>
 
-		</div>
+			<div class="address_book shipping_address_book">
+
+				<header>
+					<h3><?php esc_html_e( 'Shipping Address Book', 'woo-address-book' ); ?></h3>
+					<?php
+					// Add link/button to the my accounts page for adding addresses.
+					$wc_address_book->add_additional_address_button( 'shipping' );
+					?>
+				</header>
+
+				<p class="myaccount_address">
+					<?php echo esc_html( apply_filters( 'woocommerce_my_account_my_address_book_description', __( 'The following addresses are available during the checkout process.', 'woo-address-book' ) ) ); ?>
+				</p>
+
+				<?php
+				if ( !wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
+					echo '<div class="col2-set addresses address-book">';
+				}
+
+				foreach ( $woo_address_book_shipping_address_book as $woo_address_book_name => $woo_address_book_fields ) {
+
+					// Prevent default shipping from displaying here.
+					if ( 'shipping' === $woo_address_book_name ) {
+						continue;
+					}
+
+					$woo_address_book_address = apply_filters(
+						'woocommerce_my_account_my_address_formatted_address',
+						array(
+							'first_name' => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_first_name', true ),
+							'last_name'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_last_name', true ),
+							'company'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_company', true ),
+							'address_1'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_1', true ),
+							'address_2'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_address_2', true ),
+							'city'		 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_city', true ),
+							'state'		 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_state', true ),
+							'postcode'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_postcode', true ),
+							'country'	 => get_user_meta( $woo_address_book_customer_id, $woo_address_book_name . '_country', true ),
+						),
+						$woo_address_book_customer_id,
+						$woo_address_book_name
+					);
+
+					$woo_address_book_formatted_address = WC()->countries->get_formatted_address( $woo_address_book_address );
+
+					if ( $woo_address_book_formatted_address ) {
+						?>
+						<div class="wc-address-book-address">
+							<div class="wc-address-book-meta">
+								<a href="<?php echo esc_url( $wc_address_book->get_address_book_endpoint_url( $woo_address_book_name, 'shipping' ) ); ?>" class="wc-address-book-edit"><?php echo esc_attr__( 'Edit', 'woo-address-book' ); ?></a>
+								<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-delete"><?php echo esc_attr__( 'Delete', 'woo-address-book' ); ?></a>
+								<a id="<?php echo esc_attr( $woo_address_book_name ); ?>" class="wc-address-book-make-primary"><?php echo esc_attr__( 'Make Primary', 'woo-address-book' ); ?></a>
+							</div>
+							<address>
+								<?php echo wp_kses( $woo_address_book_formatted_address, array('br' => array()) ); ?>
+							</address>
+						</div>
+						<?php
+					}
+				}
+
+				if ( !wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
+					echo '</div>';
+				}
+				?>
+			</div>
 		<?php
-		// Add link/button to the my accounts page for adding addresses.
-		$wc_address_book->add_additional_address_button( 'shipping' );
-	endif;
-endif;
+		}
+	}
+}
