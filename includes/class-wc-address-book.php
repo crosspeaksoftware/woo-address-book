fdaa77fc6710baa8cba03451f387d98b3c48f573
Matt Harrison <matt@hallme.com>
2022-04-15 10:00:04 -0400

    require jquery-blockui as script dependency.
    Fixes #119

--- includes/class-wc-address-book.php
+++ includes/class-wc-address-book.php
@@ -181,7 +181,7 @@ class WC_Address_Book {
 	public function scripts_styles() {
 		$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 		wp_register_style( 'woo-address-book', plugins_url( "/assets/css/style$min.css", dirname( __FILE__ ) ), array(), $this->version );
-		wp_register_script( 'woo-address-book', plugins_url( "/assets/js/scripts$min.js", dirname( __FILE__ ) ), array( 'jquery' ), $this->version, true );
+		wp_register_script( 'woo-address-book', plugins_url( "/assets/js/scripts$min.js", dirname( __FILE__ ) ), array( 'jquery', 'jquery-blockui' ), $this->version, true );
 
 		wp_localize_script(
 			'woo-address-book',
