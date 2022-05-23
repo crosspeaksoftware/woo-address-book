dec8ed350a109cd20ec9153ac49208def74d655d
Matt Harrison <matt@hallme.com>
2022-05-23 13:48:15 -0400

    bump WooCommerce tested version

--- woocommerce-address-book.php
+++ woocommerce-address-book.php
@@ -8,7 +8,7 @@
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: woo-address-book
- * WC tested up to: 6.4.0
+ * WC tested up to: 6.5.1
  *
  * @package WooCommerce Address Book
  */
