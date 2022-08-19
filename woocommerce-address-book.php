e86436790782f18fbe5083bf01d59199d974d95e
Matt Harrison <matt@hallme.com>
2022-08-19 10:43:01 -0400

    Update readme with new stable tag 2.1.4

--- woocommerce-address-book.php
+++ woocommerce-address-book.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Address Book
  * Description: Gives your customers the option to store multiple shipping addresses and retrieve them on checkout..
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hall Internet Marketing
  * Author URI: https://www.hallme.com/
  * License: GPL2
